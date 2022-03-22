ad70ccadbf12fa384ccb363915d42212d006187b
DESKTOP-IRIQRDG\ADMIN <mallahsaba28@gmail.com>
2022-03-22 21:02:10 +0500

    fix concerns

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const productsCenterDOM = document.querySelector(".products-center");
 
-// get all products
-
-const getProducts = async function () {
+(async function () {
   let response = await fetch("products.json");
   let data = await response.json();
   let results = "";
@@ -26,32 +24,5 @@ const getProducts = async function () {
       </article>
   `;
   }
-  return (productsCenterDOM.innerHTML = results);
-};
-getProducts();
-
-// productsCenterDOM.innerHTML = fetch("products.json")
-//   .then((products) => products.json())
-//   .then((products) => {
-//     let results = "";
-//     for (let index = 0; index < products.length; index++) {
-//       results += `
-//      <article class="product">
-//         <div class="img-container">
-//             <img
-//                 src="${products[index].url}"
-//                 alt="product"
-//                 class="product-img"
-//             />
-//             <button class="bag-btn" data-id="1">
-//                 <i class="fas fa-shopping-cart"></i>
-//                 add to bag
-//             </button>
-//         </div>
-//         <h3>${products[index].name}</h3>
-//         <h4>$${products[index].price}</h4>
-//     </article>
-// `;
-//     }
-//     return results;
-//   });
+  productsCenterDOM.innerHTML = results;
+})();
