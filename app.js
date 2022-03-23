a0099eba83e3a74311b089c52f370ff7abab8cd8
DESKTOP-IRIQRDG\ADMIN <mallahsaba28@gmail.com>
2022-03-23 20:53:59 +0500

    removed unnecessary comments

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const cart = document.querySelector(".cart");
 const cartOverlay = document.querySelector(".cart-overlay");
 let closeCartDOM = document.querySelector(".close-cart");
 
-// get all products
-
 const getProducts = async function () {
   let response = await fetch("products.json");
   let data = await response.json();
@@ -48,29 +46,3 @@ function closeCart() {
   closeCartDOM = cart.style.transform = "translateX(100%)";
   closeCartDOM = cartOverlay.style.visibility = "hidden";
 }
-
-// productsCenterDOM.innerHTML = fetch("products.json")
-//   .then((products) => products.json())
-//   .then((products) => {
-//     let results = "";
-//     for (let index = 0; index < products.length; index++) {
-//       results += `
-//      <article class="product">
-//         <div class="img-container">
-//             <img
-//                 src="${products[index].url}"
-//                 alt="product"
-//                 class="product-img"
-//             />
-//             <button class="bag-btn" data-id="1">
-//                 <i class="fas fa-shopping-cart"></i>
-//                 add to bag
-//             </button>
-//         </div>
-//         <h3>${products[index].name}</h3>
-//         <h4>$${products[index].price}</h4>
-//     </article>
-// `;
-//     }
-//     return results;
-//   });
